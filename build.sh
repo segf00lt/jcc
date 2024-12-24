573452d20d0d9a87df2b3e105d349ea2ccd21ccf
segf00lt <joaodear2007@gmail.com>
2024-12-24 18:35:08 -0300

    fix build on mac

--- build.sh
+++ build.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
+OS_NAME=$(uname)
+
 cd ./third_party/raylib && make RAYLIB_LIBTYPE=STATIC && cd ../..;
 
 make
 
 cd ./third_party/raylib && make RAYLIB_LIBTYPE=SHARED && cd ../..;
 cp ./third_party/raylib/libraylib.a ./raylib;
-cp -P ./third_party/raylib/libraylib.so* ./raylib;
+if [ "$OS_NAME" = "Linux" ]; then
+    mv ./third_party/raylib/libraylib.so* ./raylib;
+elif [ "$OS_NAME" = "Darwin" ]; then
+    mv ./third_party/raylib/libraylib.*dylib ./raylib;
+else
+    echo "Unkown OS: $OS_NAME";
+fi
